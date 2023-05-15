838f37cc944ef43a2579bbf9fb0d130c61793aeb
duynt2499 <118056890+duynt99@users.noreply.github.com>
2023-05-15 13:07:41 +0700

    update version ruby file

--- front-end/src/configs/api-config.ts
+++ front-end/src/configs/api-config.ts
@@ -1,5 +1,5 @@
 const config = {
-  BASE_URL: "http://localhost:3000",
+  BASE_URL: import.meta.env.VITE_BASE_URL,
   client_id: import.meta.env.VITE_CLIENT_ID,
   client_secret: import.meta.env.VITE_CLIENT_SECRET,
 };
